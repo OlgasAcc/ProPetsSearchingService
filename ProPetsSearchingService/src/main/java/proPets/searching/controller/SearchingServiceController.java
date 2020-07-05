6dd07dea44c4b9b7f609414eb79afaeed4cdc674
olgak_tdvqm06 <olgak_tdvqm06@DESKTOP-U200NEI>
2020-07-05 22:28:40 +0300

    05_07_2020
    changed LocalResponseDto to ResponseDto,
    tested the methods,
    
    need to add (like option) the field Subscribed: true/false to model ->
    db ES for making the author able not to get emails with new matched post

--- ProPetsSearchingService/src/main/java/proPets/searching/controller/SearchingServiceController.java
+++ ProPetsSearchingService/src/main/java/proPets/searching/controller/SearchingServiceController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import proPets.searching.dao.SearchingServiceRepository;
-import proPets.searching.dto.LocationResponseDto;
+import proPets.searching.dto.ResponseDto;
 import proPets.searching.dto.RequestDto;
 import proPets.searching.model.PostSearchData;
 import proPets.searching.service.SearchingService;
@@ -42,11 +42,11 @@ public class SearchingServiceController {
 	}
 
 	@GetMapping("/location")
-	public ResponseEntity<LocationResponseDto> getMatchingPostsByDistance(@RequestParam("address") String address,
+	public ResponseEntity<ResponseDto> getMatchingPostsByDistance(@RequestParam("address") String address,
 			@RequestParam("flag") String flag) throws Exception {
 		String flagToSearch = flag.equalsIgnoreCase("lost") ? "found" : "lost";
 		String[] arr = searchingService.searchPostsByDistance(address, flagToSearch); // для ручного поиска по локации: на странице клиент ищет посты в той же базе
-		LocationResponseDto body = new LocationResponseDto(arr);
+		ResponseDto body = new ResponseDto(arr);
 		return ResponseEntity.ok().body(body);
 	}
 	
@@ -57,21 +57,21 @@ public class SearchingServiceController {
 	}
 		
 	//его запустит ЛФ, когда юзер нажмет на ссылку в письме, чтобы отрисовать совпавшие посты на фронте
-	@GetMapping("/all_matches")
-	public ResponseEntity<LocationResponseDto> getMatchingPosts(@RequestParam("post") String postId,
+	@GetMapping("/all_matched")
+	public ResponseEntity<ResponseDto> getMatchingPosts(@RequestParam("postId") String postId,
 			@RequestParam("flag") String flag) throws Exception {
 		String[] arr = searchingService.getPostsIdsOfMatchingPosts(postId, flag);
-		LocationResponseDto body = new LocationResponseDto(arr);
+		ResponseDto body = new ResponseDto(arr);
 		return ResponseEntity.ok().body(body);
 	}
 	
-	//ЛФ вызывает асинхронно. Ищет список авторов. Шлет 1 запрос со списком авторов в боди в мэйлинг. Второй в мэйлинг - автору нового поста
+	//??ЛФ вызывает асинхронно. Ищет список авторов. Шлет 1 запрос со списком авторов в боди в мэйлинг. Второй в мэйлинг - автору нового поста
 	// для теста - вызвать отсюда и проверить с email service
 	@GetMapping("/notification")
-	public ResponseEntity<LocationResponseDto> getMatchingPostsAuthors(@RequestParam("post") String postId,
+	public ResponseEntity<ResponseDto> getMatchingPostsAuthors(@RequestParam("post") String postId,
 			@RequestParam("flag") String flag) throws Exception {
 		String[] authorsEmails = searchingService.getAuthorsOfMatchingPosts(postId, flag);
-		LocationResponseDto body = new LocationResponseDto(authorsEmails);
+		ResponseDto body = new ResponseDto(authorsEmails);
 		return ResponseEntity.ok().body(body);
 	}
 	
--- ProPetsSearchingService/src/main/java/proPets/searching/dto/ResponseDto.java
+++ ProPetsSearchingService/src/main/java/proPets/searching/dto/ResponseDto.java
@@ -8,6 +8,6 @@ import lombok.Setter;
 @Getter
 @Setter
 
-public class LocationResponseDto {
+public class ResponseDto {
 	String[] postIds;
 }
